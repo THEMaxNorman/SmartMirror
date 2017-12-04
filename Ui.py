43bdb1992d7d47720d40b4f7930433cfc3299f33
THEMaxNorman <30306324+THEMaxNorman@users.noreply.github.com>
2017-12-03 23:05:18 -0800

    Update Function
    Added an update function that updates hourly*
    
    *when hours changes value

--- Ui.py
+++ Ui.py
@@ -8,9 +8,11 @@ import time
 from PIL import ImageTk, Image,ImageFont, ImageDraw
 import feedparser
 import json
+import freenect
+import numpy as np
 
 
-
+obj_distance = 100
 class FullScreenApp(object):
     def __init__(self, master, **kwargs):
         self.master=master
@@ -24,11 +26,12 @@ class FullScreenApp(object):
         print(geom,self._geom)
         self.master.geometry(self._geom)
         self._geom=geom
-
+public_images = []
 import smtplib
 import time
 import imaplib
 import email
+BASELINE_DEPTH = 80
 LEFT_SIDE = 1
 ORG_EMAIL   = "@gmail.com"
 FROM_EMAIL  = "maxnorman.biz" + ORG_EMAIL
@@ -41,6 +44,21 @@ SMTP_PORT   = 465
 # Utility to read email from Gmail Using Python
 #
 # ------------------------------------------------
+def get_depth():
+    array, _ = freenect.sync_get_depth()
+    array = array.astype(np.uint8)
+    checker = 0
+    for x in range(150, 350):
+        for y in range(150, 350):
+            if (array[x][y] >= BASELINE_DEPTH):
+                checker += 1
+
+    if checker >= (200 * 200) / 2:
+        addBtc()
+        addHashRate()
+        addNewsFeed()
+        print "aa"
+        images = putWeatherOnScreen()
 
 def read_email_from_gmail():
     endArr = []
@@ -74,6 +92,40 @@ def read_email_from_gmail():
 cnnUS = "http://rss.cnn.com/rss/cnn_us.rss"
 cnntech = "http://rss.cnn.com/rss/cnn_tech.rss"
 slushUrl = "https://slushpool.com/accounts/profile/json/1787598-f474765ec374ff7e6ea8695635f1415f"
+def add_images_to_public():
+    size = 60, 60
+    list_of_things = ["Sunny", "Cloudy", "Rain", "Moon", "Snow"]
+    for c in list_of_things:
+        ab = Image.open(getImage(c))
+        ab.thumbnail(size, Image.ANTIALIAS)
+        photo = ImageTk.PhotoImage(ab)
+        public_images.append(photo)
+
+def get_thumbnails(a):
+    str(a)
+    if int(time.strftime("%H")) > 6:
+        if "Cloudy" in a:
+            return public_images[1]
+        elif "Rain" in a:
+            return public_images[2]
+        elif "Sunny" in a:
+            return public_images[3]
+        elif "Snow" in a:
+            return public_images[4]
+        else:
+            return public_images[3]
+    else:
+        if "Cloudy" in a:
+            return public_images[1]
+        elif "Rain" in a:
+            return public_images[2]
+        elif "Sunny" in a:
+            return public_images[0]
+        elif "Snow" in a:
+            return public_images[4]
+        else:
+            return public_images[0]
+
 def parseRSS( rss_url ):
     return feedparser.parse( rss_url )
 def getHeadlines(rss_url):
@@ -104,9 +156,7 @@ def convertTime(a):
     b = a + 4
     print b
     if b > 12:
-        print "converting..."
         b %= 12
-    print str(b) + ":00"
     return str (b) + ":00"
 def convertTemp(a):
     return str(int(round(a, 0))) + "°"
@@ -129,18 +179,34 @@ def getWeather():
     return endArr
 def getImage(a):
     str(a)
-    if "Cloudy" in a:
-        return 'cloudy.jpeg'
-    elif "Rain" in a:
-        return 'rainy.jpeg'
-    elif "Sunny" in a:
-        return "sunny.jpeg"
+    if int(time.strftime("%H")) > 6:
+        if "Cloudy" in a:
+            return 'cloudy.jpeg'
+        elif "Rain" in a:
+            return 'rainy.jpeg'
+        elif "Sunny" in a:
+            return "moon.jpeg"
+        elif "Snow" in a:
+            return "snowy.jpeg"
+        else:
+            return "moon.jpeg"
     else:
-        return "sunny.jpeg"
+        if "Cloudy" in a:
+            return 'cloudy.jpeg'
+        elif "Rain" in a:
+            return 'rainy.jpeg'
+        elif "Sunny" in a:
+            return "moon.jpeg"
+        elif "Snow" in a:
+            return "snowy.jpeg"
+        else:
+            return "moon.jpeg"
 def putWeatherOnScreen():
     a = getWeather()
-    size = 60, 60
+
+
     images = []
+    labels = []
     start_pos = 700
     for b in a[0:6]:
 
@@ -154,20 +220,11 @@ def putWeatherOnScreen():
                             x=start_pos + (a.index(b) * 70), y=1 + (b.index(c) * 30))
                 else:
 
-                    ab = Image.open(getImage(c))
-                    ab.thumbnail(size, Image.ANTIALIAS)
-                    photo = ImageTk.PhotoImage(ab)
-                    images.append(photo)
 
-                    Label(image=photo, width=60, bg="black", fg="white").place(x = start_pos+ (a.index(b) * 70), y = 1 + (b.index(c) * 30))
+                    Label(image=get_thumbnails(c), width=60, bg="black", fg="white").place(x = start_pos+ (a.index(b) * 70), y = 1 + (b.index(c) * 30))
             else:
 
-                ab = Image.open(getImage(c))
-                ab.thumbnail(size, Image.ANTIALIAS)
-                photo = ImageTk.PhotoImage(ab)
-                images.append(photo)
-
-                Label(image=photo, width=60, bg="black", fg="white").place(x = start_pos - 7+ (a.index(b) * 70), y = 1 + (b.index(c) * 30))
+                Label(image=get_thumbnails(c), width=60, bg="black", fg="white").place(x = start_pos - 7+ (a.index(b) * 70), y = 1 + (b.index(c) * 30))
     return images
 def addBtc():
     a = Label(text=str(getBtc()),anchor = "nw", fg="white",width = 50, bg="black", font=("Helvetica Neue light", 20))
@@ -175,31 +232,56 @@ def addBtc():
 def convertFrom(a):
     c = a.index('<')
     return a[0:c]
+def take_name(str):
+    c = '"'
+    a = [pos for pos, char in enumerate(str) if char == c]
+    return str[1:a[1]]
+
+def get_all_names(a):
+    end_arr = []
+    for x in range(0, 9, 2):
+        end_arr.append(take_name(a[x]))
+
+    return end_arr
+
+def get_all_subjects(a):
+    for x in range(1, 9, 2):
+        print ((a[x]))
 def addEmails():
-    a = read_email_from_gmail()
-    for x in range (0, a.__len__(),2):
-        Label(text=str(convertFrom(a[x])), bd=2, relief="flat", fg="white", bg="black", anchor="nw", width=50, font=("Helvetica Neue light", 20)).grid(column = 7, row = x + 4)
+   a = read_email_from_gmail()
 def addNewsFeed():
     newsArr = getHeadlines(cnnUS)
     Label(text="News", fg="white", bg="black", font=("Helvetica Neue light", 40)).place(x = LEFT_SIDE, y = 100)
     for x in newsArr[0:8]:
         Label(text=str(x),bd = 2,relief = "flat", fg="white", bg="black",anchor = "nw", width=50, font=("Helvetica Neue light", 20)).place(x = LEFT_SIDE, y = 175 +(newsArr.index(x)*25) )
+
 def tick():
+
     s = time.strftime('%I:%M')
+    h = time.strftime("%I")
+    if h != clock["text"].split(":")[0]:
+        update()
     if s != clock["text"]:
         clock["text"] = s
+
+
     clock.after(200, tick)
+
+def update():
+    addBtc()
+    addHashRate()
+    addNewsFeed()
+    images = putWeatherOnScreen()
+    print "Updated"
+    return images
 root = Tk()
+add_images_to_public()
 root.configure(background = "black")
-addBtc()
-addHashRate()
-addNewsFeed()
-images = putWeatherOnScreen()
 app = FullScreenApp(root)
 clock = Label(root, fg="white", bg="black",anchor = "nw", font=("Helvetica Neue light", 80))
 clock.place(x = 700, y = 100)
 
 
 tick()
-
+images = update()
 root.mainloop()
